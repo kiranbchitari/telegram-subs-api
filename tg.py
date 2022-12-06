5630390f43c28d599c45eec68b2b5fa7367e3de1
Kiran B Chitari <kiranbchitari@gmail.com>
2022-12-05 23:49:07 -0800

    Fixing description null issue

--- tg.py
+++ tg.py
@@ -6,17 +6,20 @@ def telegram(username):
     try:
         base_url = f"https://telegram.dog/{username}"  
         r = requests.get(base_url).text
-        #print(r)
+       
         soup = bs(r,"lxml")
-        #soup.text
-        members_count = soup.find("div",class_="tgme_page_extra").text.replace(" ","").split("members")[0]
-        #print(members_count)
+       
+        members_count = soup.find("div",class_="tgme_page_extra").text.replace(" ","").split("subscribers")[0]
+      
         channel_name = soup.find("div", class_="tgme_page_title").text.replace("\n","")
-        print(channel_name)
-        description = soup.find("div",class_="tgme_page_description").text
-        #print(description)
+       
+        if soup.find("div", class_="tgme_page_description") is None:
+            description=""
+        else:    
+            description=soup.find("div", class_="tgme_page_description").text
+      
         dp = soup.find("img",class_="tgme_page_photo_image")['src']
-        #print(dp['src'])
+      
         
         data = {}
         data['name'] = channel_name #Can be used as public group also
